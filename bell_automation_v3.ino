7d37bdf7b05310c6b523b913eb0a69d065c6b339
diganta dutta <70334760+diganta121@users.noreply.github.com>
2023-07-18 15:55:07 +0530

    added power saving mode

--- bell_automation_v3.ino
+++ bell_automation_v3.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
-#include <RTClib.h>
+#include "RTClib.h"
+
 #include <LiquidCrystal_I2C.h>
 #include <SD.h>
 #include <ArduinoJson.h>
@@ -17,28 +18,13 @@ const int setting_address = 0;
 const int Lenght = 3;  // Bell ring length in seconds
 
 const int chipSelect = 15;  // SD card chip select pin
-const char* filename = "/bell_schedule.json";  // JSON file name
+const char* filename = "bell_schedule.json";  // JSON file name
 
 File configFile;
 
-int H = 0;
-int M = 0;
-int S = 0;
-
-boolean bell_status = true;
-boolean Over_ride = true;
-
-int currentAlarm = -1;
-unsigned long previousMillis = 0;
-unsigned long interval = 20 * 60 * 1000;  // Check alarm every 20 minutes
-
-int h[16] = {0};
-int m[16] = {0};
-int numAlarms = 0;
-
 const int statusLED = 13;      // Status LED pin
 const int workingLED = 12;     // Working LED pin
-const int heartbeatLED = 11;   // Heartbeat LED pin
+const int heartbeatLED = 14;   // Heartbeat LED pin
 
 const unsigned long checkInterval = 20 * 60 * 1000;  // Check nearest bell every 20 minutes
 const unsigned long halfInterval = checkInterval / 2; // Time to display bell information when halfway to the bell
@@ -46,7 +32,29 @@ const unsigned long halfInterval = checkInterval / 2; // Time to display bell in
 unsigned long lastCheckTime = 0;
 unsigned long lastDisplayTime = 0;
 
+bool heartbeatLEDState = false;
+
 bool powerSavingMode = false;
+bool setTimeMode = false;
+int setTimeState = 0; // 0: Hour, 1: Minute
+
+int H = 0;
+int M = 0;
+int S = 0;
+
+unsigned long timeTillBell = 0;
+
+boolean bell_status = true;
+boolean Over_ride = true;
+
+int currentAlarm = -1;
+
+unsigned long previousMillis = 0;
+unsigned long interval = 20 * 60 * 1000;  // Check alarm every 20 minutes
+
+int h[16] = {0};
+int m[16] = {0};
+int numAlarms = 0;
 
 void setup() {
   lcd.begin(16, 2);
@@ -73,6 +81,7 @@ void setup() {
   }
 
   if (!SD.begin(chipSelect)) {
+
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Error: SD Card");
@@ -89,12 +98,30 @@ void setup() {
   pinMode(workingLED, OUTPUT);
   pinMode(heartbeatLED, OUTPUT);
 
+  digitalWrite(statusLED, LOW);      // Turn on the status LED to indicate program running
+  digitalWrite(workingLED, LOW);     // Turn on the working LED
+  digitalWrite(heartbeatLED, LOW);    // Turn off the heartbeat LED
+  delay(500);
+
+  digitalWrite(statusLED, HIGH);      // Turn on the status LED to indicate program running
+  digitalWrite(workingLED, HIGH);     // Turn on the working LED
+  digitalWrite(heartbeatLED, HIGH);    // Turn off the heartbeat LED
+  delay(500);
+
   digitalWrite(statusLED, HIGH);      // Turn on the status LED to indicate program running
   digitalWrite(workingLED, HIGH);     // Turn on the working LED
   digitalWrite(heartbeatLED, LOW);    // Turn off the heartbeat LED
+  delay(500);
+
+  setTimeFromRTC();
 }
 
 void loop() {
+  if (setTimeMode) {
+    setTimeFromButtons();
+    return;
+  }
+
   unsigned long currentMillis = millis();
 
   if (!powerSavingMode) {
@@ -104,7 +131,6 @@ void loop() {
       updateLCD();
     }
   }
-
   // Check nearest bell every 20 minutes
   if (currentMillis - lastCheckTime >= checkInterval) {
     lastCheckTime = currentMillis;
@@ -133,13 +159,15 @@ void loop() {
         break;  // Exit the loop after the alarm is triggered
       }
     }
-  } else {
+  }
+  else {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("RTC stopped!!!");
     lcd.setCursor(0, 1);
-    lcd.print("Run SetTime code");
+    lcd.print("Press 'Select' to set time");
     blinkStatusLED(500);  // Blink status LED at 500ms intervals
+    setTimeMode = true;
     return;  // Exit the loop as there's an error with the RTC
   }
 
@@ -153,7 +181,7 @@ void over_ride() {
     lcd.setCursor(0, 0);
     lcd.print("Bell Overridden");
     lcd.setCursor(0, 1);
-    lcd.print("Press exit to quit");
+    lcd.print("Press 'Cancel' to quit");
     if (digitalRead(over_ride_off) == LOW) {
       Over_ride = false;
     }
@@ -163,7 +191,7 @@ void over_ride() {
   lcd.setCursor(0, 0);
   lcd.print("Bell is Enabled");
   lcd.setCursor(0, 1);
-  lcd.print("Exit button pressed");
+  lcd.print("Cancel button pressed");
   delay(2000);
   lcd.clear();
   saveSettings();
@@ -182,7 +210,8 @@ void activatePowerSavingMode() {
   lcd.setCursor(0, 0);
   lcd.print("Power Saving Mode");
   lcd.setCursor(0, 1);
-  lcd.print("Press Exit to quit");
+  lcd.print("Press 'Cancel' to quit");
+  unsigned long currentMillis = millis();
 
   // Check for exit button press to exit power saving mode
   while (true) {
@@ -190,13 +219,22 @@ void activatePowerSavingMode() {
       powerSavingMode = false;
       break;
     }
+
+    if (currentMillis - lastDisplayTime >= 1200000 ) {
+      lastDisplayTime = currentMillis;
+      updateLCD();
+      checkNearestBell();
+      currentMillis = millis();
+    }
+
+    blinkHeartbeatLED(5000);
   }
 
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Bell is Enabled");
   lcd.setCursor(0, 1);
-  lcd.print("Exit button pressed");
+  lcd.print("Cancel button pressed");
   delay(2000);
   lcd.clear();
   saveSettings();
@@ -209,8 +247,6 @@ void updateLCD() {
   lcd.print(formatDigits(H));
   lcd.print(":");
   lcd.print(formatDigits(M));
-  lcd.print(":");
-  lcd.print(formatDigits(S));
 
   lcd.setCursor(0, 1);
   lcd.print("Next Bell:");
@@ -239,8 +275,11 @@ void checkNearestBell() {
   }
 
   if (nearestAlarmIndex >= 0 && nearestAlarmIndex < numAlarms) {
-    unsigned long timeTillBell = nearestTimeDiff / 60;  // Convert seconds to minutes
+    timeTillBell = nearestTimeDiff / 60;  // Convert seconds to minutes
     displayTimeTillBell(timeTillBell);
+  }
+  if (timeTillBell < 1260000){
+    powerSavingMode = false;
   }
 }
 
@@ -312,6 +351,86 @@ void loadBellSchedule() {
   }
 }
 
+void setTimeFromRTC() {
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // Set the RTC to the time the sketch was compiled
+}
+
+void setTimeFromButtons() {
+  const int btnUp = 18;   // Button to increment hour
+  const int btnDown = 19; // Button to decrement hour
+  const int btnSelect = 21; // Button to switch between setting hours and minutes
+  const int btnCancel = 22; // Button to cancel setting time and resume normal operation
+
+  if (setTimeState == 0) { // Setting Hour
+    lcd.setCursor(0, 0);
+    lcd.print("Set Time: ");
+    lcd.print(formatDigits(H));
+    lcd.print(":");
+    lcd.print(formatDigits(M));
+  } else { // Setting Minute
+    lcd.setCursor(0, 0);
+    lcd.print("Set Time: ");
+    lcd.print(formatDigits(H));
+    lcd.print(":");
+    lcd.print(formatDigits(M));
+  }
+
+  if (digitalRead(btnUp) == LOW) {
+    if (setTimeState == 0) {
+      H = (H + 1) % 24;
+    } else {
+      M = (M + 1) % 60;
+    }
+    delay(250); // Debounce
+  }
+
+  if (digitalRead(btnDown) == LOW) {
+    if (setTimeState == 0) {
+      H = (H + 23) % 24;
+    } else {
+      M = (M + 59) % 60;
+    }
+    delay(250); // Debounce
+  }
+
+  if (digitalRead(btnSelect) == LOW) {
+    setTimeState = (setTimeState + 1) % 2; // Toggle between setting hours and minutes
+    delay(250); // Debounce
+  }
+
+  if (digitalRead(btnCancel) == LOW) {
+    lcd.clear();
+    setTimeMode = false;
+    setTimeFromRTC(); // Reset RTC to the time the sketch was compiled
+    return;
+  }
+
+  // Show blinking cursor for current setting (hours or minutes)
+  if (millis() % 1000 < 500) {
+    lcd.setCursor(setTimeState == 0 ? 10 : 13, 0);
+    lcd.print("_");
+  } else {
+    lcd.setCursor(setTimeState == 0 ? 10 : 13, 0);
+    lcd.print(" ");
+  }
+
+  // Save the new time when the 'Select' button is pressed and return to normal operation
+  if (digitalRead(btnSelect) == LOW) {
+    rtc.adjust(DateTime(rtc.now().year(), rtc.now().month(), rtc.now().day(), H, M, 0));
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Time set to:");
+    lcd.setCursor(0, 1);
+    lcd.print(formatDigits(H));
+    lcd.print(":");
+    lcd.print(formatDigits(M));
+    delay(2000);
+    lcd.clear();
+    setTimeMode = false;
+    return;
+  }
+}
+
 void blinkStatusLED(unsigned long interval) {
   static unsigned long previousMillis = 0;
   static bool statusLEDState = false;
@@ -323,4 +442,18 @@ void blinkStatusLED(unsigned long interval) {
     statusLEDState = !statusLEDState;
     digitalWrite(statusLED, statusLEDState ? HIGH : LOW);
   }
+}
+
+void blinkHeartbeatLED(unsigned long interval) {
+  static unsigned long previousMillis = 0;
+  static bool heartbeatLEDState = false;
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    heartbeatLEDState = !heartbeatLEDState;
+    digitalWrite(heartbeatLED, heartbeatLEDState ? HIGH : LOW);
+  }
+
 }
